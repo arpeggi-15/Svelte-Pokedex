99cae51d674a00e11a4255cbdcb17ecc21fe97f8
arpeggi-15 <priyanshu.kr432@gmail.com>
2022-03-01 00:25:24 +0530

    commit

---
+++ src/components/nav.svelte
@@ -0,0 +1,4 @@
+<nav class="flex justify-center w-full">
+    <a class=" mx-4 text-lg" href="/">HOME</a>
+    <a class=" mx-4 text-lg" href="/about">ABOUT</a>
+</nav>
---
+++ src/routes/pokemon/[id].svelte
@@ -0,0 +1,18 @@
+<script context="module">
+    export async function load({ params, fetch}){
+        const url = `https://pokeapi.co/api/v2/pokemon/${params.id}`
+        const res = await fetch(url)
+        const pokeman = await res.json()
+        return {props: {pokeman}}
+    }
+</script>
+
+<script>
+    export let pokeman
+    const type = pokeman.types[0].type.name
+</script>
+<div class="flex flex-col text-center my-8 uppercase">
+    <h1 class="text-4xl text-center my-8 uppercase">{pokeman.name}</h1>
+    <p>Type: <strong>{type}</strong> | Height: <strong>{pokeman.height}</strong> | Weight: <strong>{pokeman.weight}</strong></p>
+    <img class="card-image" src={pokeman.sprites['front_default']} alt={pokeman.name}/>
+</div>
