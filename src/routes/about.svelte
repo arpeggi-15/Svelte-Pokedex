e9ead844bfad0d0467111e6f11ce4d1d0f3587a8
arpeggi-15 <priyanshu.kr432@gmail.com>
2022-03-01 00:24:38 +0530

    initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+    <title>About Page</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">ABOUT</h1>
+
+<p class="text-center">This is a svelte application which displays all the stats about the pokemon.</p>
+
+<h3 class="text-center my-40">Made by Priyanshu</h3>
